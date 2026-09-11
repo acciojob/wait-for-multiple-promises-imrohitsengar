17641e99c6bbda322802ee367b90843961bbed66
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-11 16:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-//your JS code here. If required.
-const tbody=document.getElementById('output');
-const Promise1=new Promise((resolve)=>{
-	setTimeout(()=>{
-		resolve(2);
-	},2000)
-})
-const Promise2=new Promise((resolve)=>{
-	setTimeout(()=>{
-		resolve(3);
-	},3000)
-})
-const Promise3=new Promise((resolve)=>{
-	setTimeout(()=>{
-		resolve(1);
-	},1000)
-})
-Promise.all([Promise1,Promise2,Promise3]).then(resolvedData=>{
-	 tbody.innerHTML = "";
-	resolvedData.forEach((data,index)=>{
-		tbody.innerHTML+=`<tr>
-		                  <td>Promise ${index+1}</td>
-		                  <td>${data}</td>
-						  </tr>`
-	})
-	const total = Math.max(...resolvedData);
+const tbody = document.getElementById("output");
+
+function createPromise() {
+    return new Promise((resolve) => {
+
+        const startTime = performance.now();
+
+        // Random delay between 1 and 3 seconds
+        const delay = Math.floor(Math.random() * 3 + 1) * 1000;
+
+        setTimeout(() => {
+
+            const endTime = performance.now();
+
+            // Actual time taken in seconds
+            const timeTaken = (endTime - startTime) / 1000;
+
+            resolve(timeTaken);
+
+        }, delay);
+    });
+}
+
+const promise1 = createPromise();
+const promise2 = createPromise();
+const promise3 = createPromise();
+
+Promise.all([promise1, promise2, promise3])
+    .then((resolvedData) => {
+
+        // Remove Loading... row
+        tbody.innerHTML = "";
+
+        // Add Promise 1, Promise 2, Promise 3
+        resolvedData.forEach((data, index) => {
+
+            tbody.innerHTML += `
+                <tr>
+                    <td>Promise ${index + 1}</td>
+                    <td>${data.toFixed(3)}</td>
+                </tr>
+            `;
+        });
+        // Longest promise = total time
+        const totalTime = Math.max(...resolvedData);
+
         tbody.innerHTML += `
             <tr>
                 <td>Total</td>
-                <td>${total.toFixed(3)}</td>
+                <td>${totalTime.toFixed(3)}</td>
             </tr>
         `;
-})
+    });
