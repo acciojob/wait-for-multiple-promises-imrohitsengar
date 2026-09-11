8fc1c479c350ba3442191dc02459e876e11b4d8f
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-11 16:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const tbody=document.getElementById('output');
+const Promise1=new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(2);
+	},2000)
+})
+const Promise2=new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(3);
+	},3000)
+})
+const Promise3=new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(1);
+	},1000)
+})
+Promise.all([Promise1,Promise2,Promise3]).then(resolvedData=>{
+	 tbody.innerHTML = "";
+	resolvedData.forEach((data,index)=>{
+		tbody.innerHTML+=`<td>Promise ${index+1}</td>
+		                  <td>${data}</td>`
+	})
+	const total = Math.max(...resolvedData);
+        tbody.innerHTML += `
+            <tr>
+                <td>Total</td>
+                <td>${total.toFixed(3)}</td>
+            </tr>
+        `;
+})
